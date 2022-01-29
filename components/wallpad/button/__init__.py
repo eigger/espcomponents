73d4b1b584ffc578eed92b750a7470005ace4108
eigger <eigger87@gmail.com>
2022-01-29 17:54:46 +0900

    update

--- components/wallpad/button/__init__.py
+++ components/wallpad/button/__init__.py
@@ -1,8 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button, wallpad
-from esphome.const import CONF_ID, CONF_INVERTED, CONF_DEVICE, \
-    CONF_STATE_ON, CONF_STATE_OFF, CONF_COMMAND_OFF, CONF_COMMAND_STATE
+from esphome.const import CONF_ID, CONF_INVERTED, CONF_DEVICE
+from ..const import CONF_SUB_DEVICE, CONF_STATE_ON, CONF_STATE_OFF, CONF_COMMAND_OFF, CONF_COMMAND_STATE
 from .. import wallpad_ns
 
 DEPENDENCIES = ['wallpad']
@@ -12,6 +12,7 @@ CONFIG_SCHEMA = button.BUTTON_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(WallPadButton),
     cv.Optional(CONF_INVERTED): cv.invalid("WallPad buttons do not support inverted mode!"),
     cv.Optional(CONF_DEVICE): cv.invalid("WallPad buttons do not support device!"),
+    cv.Optional(CONF_SUB_DEVICE): cv.invalid("WallPad buttons do not support sub device!"),
     cv.Optional(CONF_STATE_ON): cv.invalid("WallPad buttons do not support state on!"),
     cv.Optional(CONF_STATE_OFF): cv.invalid("WallPad buttons do not support state off!"),
     cv.Optional(CONF_COMMAND_OFF): cv.invalid("WallPad buttons do not support command off!"),
