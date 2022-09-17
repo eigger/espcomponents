c79fa1c99054488b663e7196a852a84c01c16338
eigger <eigger87@gmail.com>
2022-09-17 23:48:10 +0900

    Update

--- components/uartex/text_sensor/__init__.py
+++ components/uartex/text_sensor/__init__.py
@@ -13,7 +13,7 @@ UARTExTextSensor = uartex_ns.class_(
 
 CONFIG_SCHEMA = cv.All(text_sensor.TEXT_SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(UARTExTextSensor),
-    #cv.GenerateID(CONF_UARTEX_ID): cv.use_id(UARTExComponent),
+    cv.GenerateID(CONF_UARTEX_ID): cv.use_id(UARTExComponent),
     cv.Required(CONF_DEVICE): state_schema,
     cv.Optional(CONF_SUB_DEVICE): state_schema,
     cv.Optional(CONF_COMMAND_STATE): command_hex_schema,
