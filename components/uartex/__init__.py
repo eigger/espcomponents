388d4b6fd4ee9f1585ba6f98b04dddd4a8aa46f8
eigger <eigger87@gmail.com>
2022-09-17 23:55:38 +0900

    Update __init__.py

--- components/uartex/__init__.py
+++ components/uartex/__init__.py
@@ -113,7 +113,7 @@ async def to_code(config):
     await uart.register_uart_device(var, config)
     if CONF_VERSION in config:
         sens = cg.new_Pvariable(config[CONF_VERSION][CONF_ID])
-        await textsensor_ns.register_text_sensor(sens, config[CONF_VERSION])
+        await textsensor_ns.text_sensor.register_text_sensor(sens, config[CONF_VERSION])
         cg.add(var.set_version(sens))
 
     
