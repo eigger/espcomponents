621104e7d36d1dfc02c7f032ffb20d5b104b24a1
eigger <eigger87@gmail.com>
2022-09-26 23:41:50 +0900

    Update uartex_lock.h

--- components/uartex/lock/uartex_lock.h
+++ components/uartex/lock/uartex_lock.h
@@ -15,8 +15,8 @@ public:
     void publish(const std::vector<uint8_t>& data) override;
     bool publish(bool state) override { return false; }
     void control(const lock::LockCall &call) override;
-    void lock() override;
-    void unlock() override;
+    void lock();
+    void unlock();
     void set_state_locked(state_t state) { state_locked_ = state; }
     void set_state_unlocked(state_t state) { state_unlocked_ = state; }
     void set_state_jammed(state_t state) { state_jammed_ = state; }
