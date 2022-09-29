fe6c0f8c4769b216b01fa78fe3d7a5d2e8b8e63d
eigger <eigger87@gmail.com>
2022-09-29 23:33:54 +0900

    Add Number

---
+++ components/uartex/number/__init__.py
@@ -0,0 +1,42 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import number, uartex
+from esphome.const import CONF_ID, CONF_MIN_VALUE, CONF_MAX_VALUE, CONF_STEP, CONF_OFFSET
+from .. import uartex_ns, cmd_t, uint8_ptr_const, uint16_const, STATE_NUM_SCHEMA
+from ..const import CONF_COMMAND_NUMBER, CONF_COMMAND_OFF, CONF_STATE_NUMBER, CONF_STATE_OFF, \
+    CONF_COMMAND_ON, CONF_STATE_ON, CONF_LENGTH, CONF_PRECISION
+
+DEPENDENCIES = ['uartex']
+UARTExNumber = uartex_ns.class_('UARTExNumber', number.Number, cg.Component)
+
+CONFIG_SCHEMA = cv.All(number.NUMBER_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(UARTExNumber),
+    cv.Required(CONF_MIN_VALUE): cv.float_,
+    cv.Required(CONF_MAX_VALUE): cv.float_,
+    cv.Required(CONF_STEP): cv.float_,
+    cv.Required(CONF_STATE_NUMBER): cv.templatable(STATE_NUM_SCHEMA),
+    cv.Required(CONF_COMMAND_NUMBER): cv.returning_lambda,
+}).extend(uartex.UARTEX_DEVICE_SCHEMA).extend({
+    cv.Optional(CONF_COMMAND_ON): cv.invalid("UARTEx Number do not support command_on!"),
+    cv.Optional(CONF_COMMAND_OFF): cv.invalid("UARTEx Number do not support command_off!"),
+    cv.Optional(CONF_STATE_ON): cv.invalid("UARTEx Number do not support state_on!"),
+    cv.Optional(CONF_STATE_OFF): cv.invalid("UARTEx Number do not support state_off!")
+}).extend(cv.COMPONENT_SCHEMA))
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield number.register_number(var, config)
+    yield uartex.register_uartex_device(var, config)
+
+    templ = yield cg.templatable(config[CONF_COMMAND_NUMBER], [(cg.float_.operator('const'), 'x')], cmd_t)
+    cg.add(var.set_command_number(templ))
+ 
+    state = config[CONF_STATE_NUMBER]
+    if cg.is_template(state):
+        templ = yield cg.templatable(state, [(uint8_ptr_const, 'data'), (uint16_const, 'len')], cg.float_)
+        cg.add(var.set_state_number(templ))
+    else:
+        args = yield state[CONF_OFFSET], state[CONF_LENGTH], state[CONF_PRECISION]
+        cg.add(var.set_state_number(args))
---
+++ components/uartex/number/uartex_number.cpp
@@ -0,0 +1,56 @@
+#include "uartex_number.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace uartex {
+
+static const char *TAG = "uartex.number";
+
+void UARTExNumber::dump_config()
+{
+    ESP_LOGCONFIG(TAG, "UARTEx Number '%s':", device_name_->c_str());
+    dump_uartex_device_config(TAG);
+}
+
+void UARTExNumber::setup()
+{
+}
+
+void UARTExNumber::publish(const std::vector<uint8_t>& data)
+{
+    if (this->state_number_func_.has_value())
+    {
+        optional<float> val = (*this->state_number_func_)(&data[0], data.size());
+        if (val.has_value() && this->state != val.value())
+        {
+            this->state = val.value();
+            this->publish_state(this->state);
+        }
+    }
+    else if (this->state_number_.has_value() && data.size() >= (this->state_number_.value().offset + this->state_number_.value().length))
+    {
+        float val = state_to_float(data, this->state_number_.value());
+        if (this->state != val)
+        {
+            this->state = val;
+            this->publish_state(this->state);
+        }
+    }
+}
+
+void UARTExNumber::control(float value)
+{
+    if (this->command_number_func_.has_value())
+    {
+        if (this->state != value)
+        {
+            cmd_t command = (*this->command_number_func_)(value);
+            push_tx_cmd(&command);
+            this->state = value;
+            this->publish_state(value);
+        }
+    }
+}
+
+}  // namespace uartex
+}  // namespace esphome
