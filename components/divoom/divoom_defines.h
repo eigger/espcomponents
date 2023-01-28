d184458d50fc7db36589c9e4a75baedd12065678
eigger <eigger87@gmail.com>
2023-01-28 17:56:39 +0900

    Update divoom_defines.h

--- components/divoom/divoom_defines.h
+++ components/divoom/divoom_defines.h
@@ -2,10 +2,10 @@
 
 namespace esphome {
 namespace divoom {
-#define VERSION "1.0.0-230127"
+#define VERSION "2.0.0-230128"
 static const uint8_t DIVOOM_HEADER  = 0x01;
 static const uint8_t DIVOOM_FOOTER  = 0x02;
-static const uint8_t MAX_WIDTH      = 8 * 50;
-static const uint8_t MAX_HEIGHT     = 64;
+static const uint32_t MAX_WIDTH      = 8 * 50;
+static const uint32_t MAX_HEIGHT     = 64;
 }  // namespace divoom
 }  // namespace esphome
