975db6a043f1d01a019d002ee7e37dbb89c4c616
eigger <eigger87@gmail.com>
2023-02-06 13:30:41 +0900

    Update divoom_defines.h

--- components/divoom/divoom_defines.h
+++ components/divoom/divoom_defines.h
@@ -2,7 +2,7 @@
 
 namespace esphome {
 namespace divoom {
-#define VERSION "2.0.3-230130"
+#define VERSION "2.1.0-230206"
 static const uint8_t DIVOOM_HEADER  = 0x01;
 static const uint8_t DIVOOM_FOOTER  = 0x02;
 }  // namespace divoom
