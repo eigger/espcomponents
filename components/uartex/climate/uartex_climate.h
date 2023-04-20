b4a0aa44b6fb97b703311d1dc661d439f65bf323
eigger <eigger87@gmail.com>
2023-04-20 23:13:45 +0900

    Update uartex_climate.h

--- components/uartex/climate/uartex_climate.h
+++ components/uartex/climate/uartex_climate.h
@@ -11,6 +11,7 @@ namespace uartex {
 class UARTExClimate : public climate::Climate, public UARTExDevice
 {
 public:
+    UARTExClimate() : Climate(), UARTExDevice() {}
     void dump_config() override;
     void setup() override;
     void publish(const std::vector<uint8_t>& data) override;
