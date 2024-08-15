99e2194ed21248483cacf7dfa34af9386a262a9e
eigger <eigger87@gmail.com>
2024-08-15 18:10:28 +0900

    Update gicisky_esl.h

--- components/gicisky_esl/gicisky_esl.h
+++ components/gicisky_esl/gicisky_esl.h
@@ -42,7 +42,6 @@ public:
     void update() override;
     void dump_config() override;
     void setup() override;
-    void loop() override;
     void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) override;
     void set_version(text_sensor::TextSensor *version) { version_ = version; }
     void set_bt_connected(binary_sensor::BinarySensor *bt_connected) { bt_connected_ = bt_connected; } 
