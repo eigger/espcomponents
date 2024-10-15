c1ac59b020698f9cb6cbf961c042599c888309a0
eigger <eigger87@gmail.com>
2024-10-15 15:53:57 +0900

    Update bmm150.cpp

--- components/bmm150/bmm150.cpp
+++ components/bmm150/bmm150.cpp
@@ -26,21 +26,20 @@ float BMM150Component::get_setup_priority() const { return setup_priority::DATA;
 
 void BMM150Component::update()
 {
-    bmm150_read_mag_data(&dev_);
-    if (this->mag_x_ != nullptr) this->mag_x_->publish_state(dev_.data.x);
-    if (this->mag_y_ != nullptr) this->mag_y_->publish_state(dev_.data.y);
-    if (this->mag_z_ != nullptr) this->mag_z_->publish_state(dev_.data.z);
+    struct bmm150_mag_data mag_data;
+    bmm150_read_mag_data(&mag_data);
+    if (this->mag_x_ != nullptr) this->mag_x_->publish_state(mag_data.x);
+    if (this->mag_y_ != nullptr) this->mag_y_->publish_state(mag_data.y);
+    if (this->mag_z_ != nullptr) this->mag_z_->publish_state(mag_data.z);
 }
 
 int8_t BMM150Component::bmm150_initialization()
 {
     int8_t rslt = BMM150_OK;
     dev_.intf = BMM150_I2C_INTF; //SPI or I2C interface setup.
-    auto read = std::bind(&BMM150Component::i2c_read, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
-    auto write = std::bind(&BMM150Component::i2c_write, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
-    dev_.read = read;    //Read the bus pointer.
-    dev_.write = write;  //Write the bus pointer.
-    dev_.delay_ms = delay;
+    dev_.read = std::bind(&BMM150Component::i2c_read, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);    //Read the bus pointer.
+    dev_.write = std::bind(&BMM150Component::i2c_write, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);;  //Write the bus pointer.
+    dev_.delay_us = std::bind(&BMM150Component::delay_us, this, std::placeholders::_1, std::placeholders::_2);;
 
     // Set the maximum range range
     mag_max_.x = -2000;
@@ -53,10 +52,10 @@ int8_t BMM150Component::bmm150_initialization()
     mag_min_.z = 2000;
 
     rslt = bmm150_init(&dev_);   //Memory chip ID.
-    dev_.settings.pwr_mode = BMM150_NORMAL_MODE;
-    rslt |= bmm150_set_op_mode(&dev_);   //Set the sensor power mode.
-    dev_.settings.preset_mode = BMM150_PRESETMODE_ENHANCED;
-    rslt |= bmm150_set_presetmode(&dev_);    //Set the preset mode of
+    // dev_.settings.pwr_mode = BMM150_NORMAL_MODE;
+    // rslt |= bmm150_set_op_mode(&dev_);   //Set the sensor power mode.
+    // dev_.settings.preset_mode = BMM150_PRESETMODE_ENHANCED;
+    // rslt |= bmm150_set_presetmode(&dev_);    //Set the preset mode of
     return rslt;
 }
 
@@ -68,6 +67,11 @@ int8_t BMM150Component::i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *read
 int8_t BMM150Component::i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *read_data, uint16_t len)
 {
     this->write_bytes(reg_addr, read_data, len);
+}
+
+void BMM150Component::delay_us(uint32_t period_us, void *intf_ptr)
+{
+    delay(period_us);
 }
 
 }
