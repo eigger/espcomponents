dd7e0f3a1fdd53bd1b410b77d37de02a3c887ec7
eigger <eigger87@gmail.com>
2025-03-17 13:54:58 +0900

    Update automation.h

--- components/uartex/automation.h
+++ components/uartex/automation.h
@@ -11,7 +11,7 @@ class TxTimeoutTrigger : public Trigger<> {
   explicit TxTimeoutTrigger(UARTExComponent *parent) {
     parent->add_on_error_callback([this](ERROR error) {
       if (error == ERROR_ACK) this->trigger();
-      ESP_LOGD(TAG, "On Error %d", error);
+      ESP_LOGD("uartex", "On Error %d", error);
     });
   }
 };
