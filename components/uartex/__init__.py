5f31bb74e5c505ac59a77d1137ef773dfac90b13
eigger <eigger87@gmail.com>
2025-03-29 12:16:28 +0900

    Update __init__.py

--- components/uartex/__init__.py
+++ components/uartex/__init__.py
@@ -375,14 +375,14 @@ def header_hex_expression(conf):
     mask = conf[CONF_MASK]
     return data, mask
 
-def state_hex_expression(conf):
+def state_hex_expression(conf) -> state_t:
     if conf is None:
         return
     data = conf[CONF_DATA]
     mask = conf[CONF_MASK]
     inverted = conf[CONF_INVERTED]
     offset = conf[CONF_OFFSET]
-    return state_t(offset, inverted, data, mask)
+    return offset, inverted, data, mask
 
 def state_num_hex_expression(conf):
     if conf is None:
